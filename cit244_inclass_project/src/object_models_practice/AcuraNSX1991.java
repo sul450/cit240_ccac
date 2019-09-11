b9d1260b4b3a3fc670901a44fc4978c49c45c720
Liu <sliu11@ccac.org>
2019-09-11 16:15:30 -0400

    classes

---
+++ cit244_inclass_project/src/object_models_practice/AcuraNSX1991.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package object_models_practice;
+
+/**
+ *
+ * @author sliu11
+ */
+public class AcuraNSX1991 extends Vehicle {
+    
+    
+}
